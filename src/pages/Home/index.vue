476126292748ee034a3e854b342228222eff2d94
陈倩 <1940714537@qq.com>
2020-11-18 08:55:20 +0800

    登入结构完成

---
+++ src/pages/Home/index.vue
@@ -0,0 +1,148 @@
+<template>
+  <div class="home-page">
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside width="200px"
+        ><el-radio-group v-model="isCollapse" style="margin-bottom: 20px">
+          <el-radio-button :label="false">展开</el-radio-button>
+          <el-radio-button :label="true">收起</el-radio-button>
+        </el-radio-group>
+        <el-menu
+          default-active="1-4-1"
+          class="el-menu-vertical-demo"
+          @open="handleOpen"
+          @close="handleClose"
+          :collapse="isCollapse"
+        >
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span slot="title">管理首页</span>
+            </template>
+            <el-menu-item-group>
+              <span slot="title">分组一</span>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="分组2">
+              <el-menu-item index="1-3">选项3</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="1-4">
+              <span slot="title">选项4</span>
+              <el-menu-item index="1-4-1">选项1</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-menu-item index="2">
+            <i class="el-icon-menu"></i>
+            <span slot="title">学员管理</span>
+          </el-menu-item>
+          <el-menu-item index="3">
+            <i class="el-icon-document"></i>
+            <span slot="title">我的中心</span>
+          </el-menu-item>
+          <!-- <el-menu-item index="4">
+            <i class="el-icon-setting"></i>
+            <span slot="title">导航四</span>
+          </el-menu-item> -->
+        </el-menu>
+      </el-aside>
+      <el-container>
+        <!-- 顶部栏 -->
+        <el-header>
+          <el-row type="flex" class="row-bg" justify="space-between">
+            <el-col :span="6"
+              ><div class="grid-content bg-purple"></div
+            ></el-col>
+            <el-col :span="6"
+              ><div class="grid-content bg-purple-light"></div
+            ></el-col>
+            <el-col :span="6"
+              ><div class="grid-content bg-purple"></div
+            ></el-col>
+          </el-row>
+        </el-header>
+        <!-- 主体区域 -->
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      isCollapse: true,
+    };
+  },
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    },
+  },
+};
+</script>
+
+
+<style>
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 200px;
+  min-height: 400px;
+}
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.el-row {
+  margin-bottom: 20px;
+}
+.el-row:last-child {
+  margin-bottom: 0;
+}
+.el-col {
+  border-radius: 4px;
+}
+.bg-purple-dark {
+  background: #99a9bf;
+}
+.bg-purple {
+  background: #d3dce6;
+}
+.bg-purple-light {
+  background: #e5e9f2;
+}
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+.row-bg {
+  padding: 10px 0;
+  background-color: #f9fafc;
+}
+</style>
+
+
---
+++ src/pages/Login/index.vue
@@ -0,0 +1,177 @@
+<template>
+  <div class="about">
+    <div class="right">
+      <el-form
+        :model="loginForm"
+        status-icon
+        :rules="rules"
+        ref="loginForm"
+        label-width="100px"
+        class="demo-loginForm"
+      >
+        <h1 class="biaoti">千锋管理系统</h1>
+        <el-form-item label="用户名" prop="username">
+          <el-input
+            type="text"
+            v-model="loginForm.username"
+            autocomplete="off"
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input
+            type="password"
+            v-model="loginForm.password"
+            autocomplete="off"
+          ></el-input>
+        </el-form-item>
+
+        <el-form-item>
+          <el-button
+            type="primary"
+            @click="submitForm('loginForm')"
+            class="s_btn"
+            >提交</el-button
+          >
+        </el-form-item>
+      </el-form>
+    </div>
+
+    <video
+      autoplay
+      muted
+      loop
+      preload="auto"
+      src="./../../img/bg_video.d2d602a9.mp4"
+    ></video>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    //jsDoc
+    /**
+     * @param {Object} rule 就是一个表单验证对象
+     * @param {String} value 输入值
+     * @param {Function} callback  校验通过不传参  通过传参
+     */
+
+    var validateUsername = (rule, value, callback) => {
+      var uPattern = /^[a-zA-Z0-9_-]{4,16}$/;
+      if (!value) {
+        callback("4到6位(字母，数字，下划线，减号)");
+      } else {
+        callback();
+      }
+
+      // if (value === '') {
+      //   callback(new Error('请输入用户名'));
+      // } else {
+      //   if (this.loginForm.password !== '') {
+      //     this.$refs.loginForm.validateField('password');
+      //   }
+      //   callback();
+      // }
+    };
+    var validatePassword = (rule, value, callback) => {
+      if (!value) {
+        callback("请输入密码");
+      } else {
+        callback();
+      }
+
+      // if (value === '') {
+      //   callback(new Error('请输入密码'));
+      // } else if (value !== this.loginForm.username) {
+      //   callback(new Error('两次输入密码不一致!'));
+      // } else {
+      //   callback();
+      // }
+    };
+    return {
+      loginForm: {
+        username: "",
+        password: "",
+      },
+      rules: {
+        username: [{ validator: validateUsername, trigger: "blur" }],
+        password: [{ validator: validatePassword, trigger: "blur" }],
+      },
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      // console.log(this.$refs[formName])
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          //代表本地校验通过
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+  },
+};
+</script>
+
+
+<style scoped>
+/*表单样式*/
+.el-form {
+  width: 400px;
+}
+
+.s_btn {
+  width: 255px;
+  background: linear-gradient(90deg, #1596fb, #002dff);
+}
+
+.biaoti {
+  color: white;
+  font-size: 35px;
+  text-align: center;
+  margin-top: -20px;
+  margin-right: -50px;
+  padding-bottom: 40px;
+}
+.about {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  /* background: url("E:\nj-2006-STEP2\陈倩\stu-app\src\img\"); */
+}
+
+.demo-loginForm {
+  position: absolute;
+  z-index: 10;
+  float: right;
+  width: 355px;
+  height: 390px;
+  margin-top: 30px;
+  margin-left: 60%;
+  padding-top: 100px;
+  padding-right: 45px;
+  background-color: rgba(59, 52, 52, 0.3);
+  border-radius: 14px;
+}
+
+body {
+  margin: 0;
+  height: 100%;
+  position: relative;
+  overflow: hidden;
+}
+video {
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  min-width: 100%;
+  min-height: 100%;
+  width: auto;
+  height: auto;
+}
+</style>
+
